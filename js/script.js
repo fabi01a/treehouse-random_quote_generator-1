ede973019643d6bc500d03c30ff6a85271dfe6cb
Fabiola <123507148+fabi01a@users.noreply.github.com>
2026-09-07 18:25:06 -0700

    Added extra credit background color changes, style tags, and time interval changes successfully

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
-
+/*** Your feedback is greatly appreciated - I am aiming for all the "exceeds" requirement but I don't mind if I pass with a Meets Expectations grade (your feedback is what I am interested in). */
 
 /*** 
- * these are famous quotes listed in an array of objects. Each object has a `quote` and `source` property, and some objects have an optional `citation` and `year` property.
+ * These are 10 famous quotes listed in an array of objects. Each object has a `quote` and `source` property, and some objects have an optional `citation` and `year` property.
 ***/
 const quotes = [
   {
@@ -17,11 +17,13 @@ const quotes = [
   {
     quote: "You must be the change you wish to see in the world.",
     source: "Mahatma Gandhi",
-    year: "1913"
+    year: "1913",
+    tag: "inspirational"
   },
   {
     quote:  "Believe you can and you're halfway there.",
-    source: "Theodore Roosevelt"
+    source: "Theodore Roosevelt",
+    tag: "motivational"
   },
   {
     quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
@@ -59,45 +61,75 @@ const quotes = [
   }
 ];
 
-
 /***
- * this is a `getRandomQuote` function that generates a random number and returns a random quote object from the `quotes` array.
+ * This is a `getRandomQuote` function that generates a random number at the max of the `quotes` array length,  and returns a random quote object from the `quotes` array based on that number.
 ***/
 function getRandomQuote() {
   let randomNumber = Math.floor(Math.random() * quotes.length);
   return quotes[randomNumber];
 }
 
+/*** creating a function to change the background color of the page when a new quote is displayed ***/
+function changeBackgroundColor() {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
+}
+
 /***
- * `printQuote` function
+ * The `printQuote` function contains the logic to display a random quote on the webpage. It calls the `getRandomQuote` function to get a random quote object, constructs an HTML string with the quote and its properties, and updates the `quote-box` div with the generated HTML.
 ***/
 function printQuote() {
-    /***
-   * the `getRandomQuote` function will be called and stored in this variable 'randomQuote' to be used in the `printQuote` function
+  /***
+   * the `getRandomQuote` function will be called and stored in this variable 'randomQuote,' to be used in the `printQuote` function
   ***/
   const randomQuote = getRandomQuote();
 
-  /*** the 'quote' variable will be used to store the HTML string that will be displayed in the `quote-box` div. The HTML string will include the quote and source, and if available, the citation and year.
+  /*** The 'quote' variable will be used to store the HTML string that will be displayed in the `quote-box` div. The HTML string will include the quote and source, and if available, the citation and year.
   ***/
   let quote = `
     <p class="quotes">${randomQuote.quote}</p>
     <p class="source">${randomQuote.source}
   `;
 
+  /*** if the `randomQuote` object has a `citation` property, it will be added to the HTML string. ***/
   if  (randomQuote.citation) {
     quote += `<span class="citation">${randomQuote.citation}</span>`;
   }
+
+  /*** if the `randomQuote` object has a `year` property, it will be added to the HTML string. ***/
   if (randomQuote.year) {
     quote += `<span class="year">${randomQuote.year}</span>`;
   }
+
+  if (randomQuote.tag) {
+    quote += `<span class="tag">${randomQuote.tag}</span>`;
+  }
+
+  /*** the closing `</p>` tag will be added to the HTML string. ***/
   quote += `</p>`;
-  
+
+  /*** Logging the random quote object as instructed ***/
+  console.log(randomQuote);
+
+  /*** Logging one or more of the properties from the random quote object as instructed ***/
+  console.log(randomQuote.quote);
+  console.log(randomQuote.source);
+
+  /*** Logging the complete HTML string as instructed ***/
+  console.log(quote);
+
   /*** the 'quote-box' div will be updated with the HTML string stored in the 'quote' variable. ***/
   document.getElementById('quote-box').innerHTML = quote; 
-  console.log(randomQuote);
+
+  /*** calling the changeBackgroundColor function within the printQuote function to change the background color when a new quote is displayed ***/
+  changeBackgroundColor();
 };
 
 
+/*** 
+ * This is a built-in `setInterval` function that will call the `printQuote` function every 10 seconds (10000 milliseconds) to automatically display a new quote and change the background color.
+***/
+setInterval(printQuote, 10000);   
 
 
 /***
