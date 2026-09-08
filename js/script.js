035e498e9d98c47638df1854b94c6206abd09858
Fabiola <123507148+fabi01a@users.noreply.github.com>
2026-09-07 17:23:00 -0700

    updated the property names to not be individually unique to keep frontend from parsing as undefined (works successfully now)

--- js/script.js
+++ js/script.js
@@ -9,53 +9,53 @@ project 1 - A Random Quote Generator
 ***/
 const quotes = [
   {
-  quote1: "The only way to do great work is to love what you do.",
-  source1: "Steve Jobs",
-  citation1: "https://www.brainyquote.com/quotes/steve_jobs_121993",
-  year1: "2005"
+  quote: "The only way to do great work is to love what you do.",
+  source: "Steve Jobs",
+  citation: "https://www.brainyquote.com/quotes/steve_jobs_121993",
+  year: "2005"
   },
   {
-    quote2: "You must be the change you wish to see in the world.",
-    source2: "Mahatma Gandhi",
-    year2: "1913"
+    quote: "You must be the change you wish to see in the world.",
+    source: "Mahatma Gandhi",
+    year: "1913"
   },
   {
-    quote3:  "Believe you can and you're halfway there.",
-    source3: "Theodore Roosevelt"
+    quote:  "Believe you can and you're halfway there.",
+    source: "Theodore Roosevelt"
   },
   {
-    quote4: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-    source4: "Winston Churchill",
-    year4: "1941",
-    citation4: "https://www.brainyquote.com/quotes/winston_churchill_121993"
+    quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    source: "Winston Churchill",
+    year: "1941",
+    citation: "https://www.brainyquote.com/quotes/winston_churchill_121993"
   },
   {
-    quote5: "In the middle of every difficulty lies opportunity.",
-    source5: "Albert Einstein",
-    citation5: "https://www.brainyquote.com/quotes/albert_einstein_121993"
+    quote: "In the middle of every difficulty lies opportunity.",
+    source: "Albert Einstein",
+    citation: "https://www.brainyquote.com/quotes/albert_einstein_121993"
   },
   {
-    quote6: "The best way to predict the future is to invent it.",
-    source6: "Alan Kay",
-    year6: "1971"
+    quote: "The best way to predict the future is to invent it.",
+    source: "Alan Kay",
+    year: "1971"
   },
   {
-    quote7: "Do not wait to strike till the iron is hot; but make it hot by striking.",
-    source7: "William Butler Yeats"
+    quote: "Do not wait to strike till the iron is hot; but make it hot by striking.",
+    source: "William Butler Yeats"
   },
   {
-    quote8: "The journey of a thousand miles begins with one step.",
-    source8: "Lao Tzu"
+    quote: "The journey of a thousand miles begins with one step.",
+    source: "Lao Tzu"
   },
   {
-    quote9: "What you get by achieving your goals is not as important as what you become by achieving your goals.",
-    source9: "Zig Ziglar",
-    citation9: "https://www.brainyquote.com/quotes/zig_ziglar_121993",
-    year9: "1980"
+    quote: "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+    source: "Zig Ziglar",
+    citation: "https://www.brainyquote.com/quotes/zig_ziglar_121993",
+    year: "1980"
   },
   {
-    quote10: "The only limit to our realization of tomorrow will be our doubts of today.",
-    source10: "Franklin D. Roosevelt"
+    quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+    source: "Franklin D. Roosevelt"
   }
 ];
 
